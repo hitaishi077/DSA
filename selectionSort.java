9170985aafb94df6d61ef522cd0d07e318b081ff
hitaishi077 <pandeyhitaishi077@gmail.com>
2025-08-01 06:44:10 +0530

    Added SelectionSort.java

---
+++ selectionSort.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class selectionSort{
+    public static int[] selectionSort(int arr[],int n){
+        int i,j;
+
+        for(i=0;i<n;i++){
+            for(j=i;j<n;j++){
+              if(arr[i]>arr[j]){
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+
+              }  
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args){
+        int a[],n,i,z[];
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        n = sc.nextInt();
+        System.out.println("Enter the elements of array: ");
+        a = new int[n];
+        for(i=0;i<n;i++){
+            a[i] = sc.nextInt();
+
+        }
+        z=selectionSort(a, n);
+        System.out.println("Sorted array:");
+        for(i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
